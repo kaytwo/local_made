59ee67e451ac1a3764811a5c2155443e85473378
Emily O'Heron <89399844+emilyoheron@users.noreply.github.com>
2023-11-16 08:57:37 -0600

    url change

--- src/app/auth-form.tsx
+++ src/app/auth-form.tsx
@@ -2,7 +2,7 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from './lib/database.types'
+import { Database } from '@/app/lib/database.types'
 
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>();
@@ -14,7 +14,7 @@ export default function AuthForm() {
       appearance={{ theme: ThemeSupa }}
       theme="dark"
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo="https://incomparable-llama-1b3592.netlify.app/auth/callback"
     />
   )
 }
