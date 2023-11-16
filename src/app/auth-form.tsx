5aec743699c9bffb8fd135a61f4c1211f9ead841
Chris Kanich <kaytwo@gmail.com>
2023-11-16 16:10:43 -0600

    add logout button

--- src/app/auth-form.tsx
+++ src/app/auth-form.tsx
@@ -3,9 +3,34 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/app/lib/database.types'
+import { useEffect, useState } from 'react'
+import { Session } from '@supabase/auth-helpers-nextjs'
 
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>();
+  const [session, setSession] = useState<Session|null>(null)
+
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  const logOut = async () => {
+    await supabase.auth.signOut()
+
+  };
+
+  if (!session) {
 
   return (
     <Auth
@@ -14,8 +39,17 @@ export default function AuthForm() {
       appearance={{ theme: ThemeSupa }}
       theme="dark"
       providers={[]}
-      //redirectTo="http://localhost:3000/auth/callback"
-      redirectTo="https://incomparable-llama-1b3592.netlify.app/auth/callback"
+      redirectTo={`http://localhost:3000/auth/callback`}
     />
   )
+  }
+  else {
+    return (
+      <div>
+        <h1>Logged in</h1>
+        <button onClick={logOut}>Log out</button>
+
+      </div>
+    )
+  }
 }
